3f934d6e4aa89617a97918516eb8952166dc6b77
JoseCoscu <coscujjose@gmail.com>
2025-04-29 02:38:11 -0400

    [IMP] ae_crm_socials: Add customer web search by name or social account

--- ae_crm_socials/controllers/customers.py
+++ ae_crm_socials/controllers/customers.py
@@ -6,7 +6,18 @@ class Customers(http.Controller):
 
     @http.route('/customers', type='http', auth='public', website=True)
     def list_customers(self):
+        search = request.params.get('search')
         customers = request.env['res.partner'].sudo().search([])
+
+        if search:
+            domain = ['|', '|',
+                       ('name', 'ilike', search),
+                       ('email', 'ilike', search),
+                       ('comment', 'ilike', search)]
+
+            customers = request.env['res.partner'].sudo().search(domain)
+
+
         return request.render('ae_crm_socials.customers_page', {
             'customers': customers,
         })
